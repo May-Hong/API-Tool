e4f71c2705f6fcab2c13d41b65cf3ca4a730f228
May-Hong <may.hong@ylly.com>
2015-11-16 11:50:17 +0800

    try to make the result page could be visited

--- urls.py
+++ urls.py
@@ -4,11 +4,13 @@ from handlers.index import MainHandler
 from handlers.index import ResultHandler
 from handlers.index import EditHandler
 from handlers.index import SaveHandler
+from handlers.index import ReportHandler
+from tornado.web import StaticFileHandler
+
 urls = [
     (r'/', MainHandler),
     (r'/result', ResultHandler),
     (r'/edit',EditHandler),
-    (r'/save',SaveHandler)
+    (r'/save',SaveHandler),
+    (r"/report/(\w+)", ReportHandler),
 ]
-
-
